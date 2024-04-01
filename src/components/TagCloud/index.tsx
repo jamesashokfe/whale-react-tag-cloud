e6cb93c70e6d456b754d12abc951782f445b8ba8
James Ashok <jamesashok93@gmail.com>
2024-04-01 18:58:42 +0530

    Tag Cloud component - initial.

---
+++ src/components/TagCloud/index.tsx
@@ -0,0 +1,9 @@
+import "./tag-cloud.css";
+import TagCloudProps from "./tag-cloud.props";
+
+function TagCloud({ tags }: TagCloudProps) {
+  console.log(tags);
+  return <div>Hello World!</div>;
+}
+
+export default TagCloud;
---
+++ src/components/TagCloud/tag-cloud.props.ts
@@ -0,0 +1,7 @@
+import TagProps from "../Tag/tag.props";
+
+type TagCloudProps = {
+  tags: TagProps[]
+};
+
+export default TagCloudProps;
