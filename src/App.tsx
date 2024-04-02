dee6d90471c6489836b20c136e6dfd0328a5c324
James Ashok <jamesashok93@gmail.com>
2024-04-02 18:18:31 +0530

    Added TagCloud to App with demo data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import TagCloud from './components/TagCloud'
+import { tags } from './tags.mock-data'
 
 function App() {
   return (
     <>
       <h1>React Tag Cloud</h1>
-      <div></div>
+      <TagCloud tags={tags} />
     </>
   )
 }
---
+++ src/tags.mock-data.ts
@@ -0,0 +1,10 @@
+import TagProps from "./components/Tag/tag.props";
+
+export const tags: TagProps[] = [
+  {
+    text: '',
+    link: '',
+    weight: 0,
+    color: '',
+  }
+];
