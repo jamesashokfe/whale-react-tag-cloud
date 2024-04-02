e244924212a9b856d85b59199e87d86fe834a85c
James Ashok <jamesashok93@gmail.com>
2024-04-02 18:18:55 +0530

    Added demo data for tags.

--- src/tags.mock-data.ts
+++ src/tags.mock-data.ts
@@ -2,9 +2,107 @@ import TagProps from "./components/Tag/tag.props";
 
 export const tags: TagProps[] = [
   {
-    text: '',
-    link: '',
-    weight: 0,
-    color: '',
-  }
+    text: "Google",
+    link: "https://www.google.com",
+    weight: 10,
+    color: "#DB4437",
+  },
+  {
+    text: "Bing",
+    link: "https://www.bing.com",
+    weight: 9,
+    color: "#F25022",
+  },
+  {
+    text: "Apple",
+    link: "https://www.apple.com",
+    weight: 8,
+    color: "#555555",
+  },
+  {
+    text: "Microsoft",
+    link: "https://www.microsoft.com",
+    weight: 7,
+    color: "#7FBA00",
+  },
+  {
+    text: "Wordpress",
+    link: "https://www.wordpress.org",
+    weight: 8,
+    color: "#21759B",
+  },
+  {
+    text: "Drupal",
+    link: "https://www.drupal.org",
+    weight: 8,
+    color: "#0678BE",
+  },
+  {
+    text: "Search Engines",
+    weight: 12,
+    color: "#999999",
+  },
+  {
+    text: "B2B Marketing",
+    weight: 8,
+    color: "#0000CC",
+  },
+  {
+    text: "Multinational Design Agency",
+    weight: 7,
+    color: "#0000CC",
+  },
+  {
+    text: "Technology & Services",
+    color: "#990000",
+  },
+  {
+    text: "E-Commerce",
+    color: "#009900",
+  },
+  {
+    text: "Ad Optimisations",
+    color: "#000099",
+  },
+  {
+    text: "Experience Design",
+    color: "#999900",
+  },
+  {
+    text: "Social Media",
+    weight: 14,
+    color: "#990099",
+  },
+  {
+    text: "Twitter",
+    link: "https://www.twitter.com",
+    weight: 12,
+    color: "#1DA1F2",
+  },
+  {
+    text: "Facebook",
+    link: "https://www.facebook.com",
+    weight: 12,
+    color: "#4267B2",
+  },
+  {
+    text: "Instagram",
+    link: "https://www.instagram.com",
+    weight: 12,
+    color: "#C13584",
+  },
+  {
+    text: "Pinterest",
+    link: "https://www.pinterest.com",
+    weight: 12,
+    color: "#E60023",
+  },
+  {
+    text: "Performance Optimisations",
+    weight: 6,
+  },
+  {
+    text: "Accessibility Advocate",
+    weight: 7,
+  },
 ];
