bf417ae1084e9e37d4245c6b575e4d84a0cc817d
James Ashok <jamesashok93@gmail.com>
2024-04-02 18:19:26 +0530

    Add Tag Cloud configurable options.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import './App.css'
-import TagCloud from './components/TagCloud'
-import { tags } from './tags.mock-data'
+import "./App.css";
+import TagCloud from "./components/TagCloud";
+import { TagCloudOptions } from "./components/TagCloud/tag-cloud.props";
+import { tags } from "./tags.mock-data";
 
 function App() {
+  const options: TagCloudOptions = {
+    minFontSize: 12,
+    maxFontSize: 48,
+  };
+
   return (
     <>
       <h1>React Tag Cloud</h1>
-      <TagCloud tags={tags} />
+      <TagCloud tags={tags} options={options} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TagCloud/tag-cloud.props.ts
+++ src/components/TagCloud/tag-cloud.props.ts
@@ -1,7 +1,13 @@
 import TagProps from "../Tag/tag.props";
 
 type TagCloudProps = {
-  tags: TagProps[]
+  tags: TagProps[],
+  options: TagCloudOptions,
 };
+
+export class TagCloudOptions {
+  minFontSize: number = 12;
+  maxFontSize: number = 36;
+}
 
 export default TagCloudProps;
