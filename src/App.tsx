c4d5993886d63e919f0561d923be1a1c63592c61
James Ashok <jamesashok93@gmail.com>
2024-04-06 17:37:33 +0530

    React tag-cloud demo setup.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,30 @@ import { TagCloudOptions } from "./components/TagCloud/tag-cloud.props";
 import { tags } from "./tags.mock-data";
 
 function App() {
-  const options: TagCloudOptions = {
+  const optionsDemo1: TagCloudOptions = {
     maxFontSize: 48,
+  };
+  const optionsDemo2: TagCloudOptions = {
+    maxFontSize: 24,
   };
 
   return (
     <>
-      <h1>React Tag Cloud</h1>
-      <TagCloud tags={tags} options={options} />
+      <h1 className="app__title">React Tag Cloud</h1>
+
+      <div className="demo">
+        <h2 className="demo__title">Demo 1 - Large Container</h2>
+        <div className="demo__container demo__container--1">
+          <TagCloud tags={tags} options={optionsDemo1} />
+        </div>
+      </div>
+
+      <div className="demo">
+        <h2 className="demo__title">Demo 2 - Small Container</h2>
+        <div className="demo__container demo__container--2">
+          <TagCloud tags={tags} options={optionsDemo2} />
+        </div>
+      </div>
     </>
   );
 }
